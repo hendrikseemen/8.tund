ad3b8ec6b48cddb60588d11fa65d3099498a3fb8
Hendrik Seemen <hendrik7@greeny.cs.tlu.ee>
2015-10-28 11:25:46 +0200

    added samples

---
+++ page_one.php
@@ -0,0 +1,9 @@
+<?php
+
+	require_once("session.php");
+	
+	$_SESSION["name"] = "Hendrik";
+	
+	echo $_SESSION["name"];
+
+?>
---
+++ page_two.php
@@ -0,0 +1,6 @@
+<?php
+	require_once("session.php")
+	
+	echo $_SESSION["name"];
+	
+?>
